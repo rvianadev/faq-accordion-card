1decf451112226ffabaa45e13a0cd54b8781bafa
Rodrigo Viana <roviol@gmail.com>
2022-03-02 15:19:58 -0300

    faq-wrapper text adjustments

--- src/js/script.js
+++ src/js/script.js
@@ -1,11 +1,17 @@
-var arrow = document.querySelector('.icon-arrow-rotated');
-
-arrow.addEventListener('click', function () {
+function showAnswer() {
   var div = document.querySelector('.faq-answer');
 
-  if (div.style.display === 'none') {
-    div.style.display = 'block';
-  } else {
-    div.style.display = 'none';
-  }
-});
+  div.style.display === 'block'
+    ? (div.style.display = 'none')
+    : (div.style.display = 'block');
+
+  arrow.classList.toggle('icon-arrow-up');
+  question.classList.toggle('strong-question');
+
+  console.log('Fui clicado!');
+}
+
+var arrow = document.querySelector('.icon-arrow');
+var question = document.querySelector('span');
+
+arrow.addEventListener('click', showAnswer);
