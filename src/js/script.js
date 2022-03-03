1c83b1749d4d6b363d976d9801b9a93dbc5b5cf6
Rodrigo Viana <roviol@gmail.com>
2022-03-02 23:50:25 -0300

    js war

--- src/js/script.js
+++ src/js/script.js
@@ -1,17 +1,1 @@
-function showAnswer() {
-  var div = document.querySelector('.faq-answer');
-
-  div.style.display === 'block'
-    ? (div.style.display = 'none')
-    : (div.style.display = 'block');
-
-  arrow.classList.toggle('icon-arrow-up');
-  question.classList.toggle('strong-question');
-
-  console.log('Fui clicado!');
-}
-
-var arrow = document.querySelector('.icon-arrow');
-var question = document.querySelector('span');
-
-arrow.addEventListener('click', showAnswer);
+console.log('oi');
